ffd84b489a80e242d2f2912bf45aa2d8666035f0
auribe1 <ariel.uribe11@gmail.com>
2022-07-27 18:03:25 -0400

    in progress of adding function to copy passwords to clipboard on click

--- passGen.js
+++ passGen.js
@@ -1,6 +1,8 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
+passwordsExist = false
+
 let pass = ""
 let passwordEl = document.getElementById("password-el")
 let pass2 = ""
@@ -24,6 +26,13 @@ function generatePass(){
     }
     passwordEl.textContent = pass
     passwordEl2.textContent = pass2
+    passwordsExist = true
+}
+
+//in progress of adding copy text on click function
+function copyText(){
+    var copyText1 = document.getElementById('password-el')
+    var copyText2 = document.getElementById('passwordEl2')
 }
 
 //TODO: add feature to input a password length
